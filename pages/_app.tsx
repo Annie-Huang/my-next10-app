4aeeb4f5a77ce22aa8b1966b08c427189f0031ba
Annie Huang <annie.hn@gmail.com>
2021-06-02 21:44:41 +1000

    Add global style

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,29 +1,42 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
 
 import App from 'next/app';
-import {ThemeProvider} from 'styled-components';
+import { createGlobalStyle, ThemeProvider } from 'styled-components';
 
 // function MyApp({ Component, pageProps }: AppProps) {
 //   return <Component {...pageProps} />
 // }
 // export default MyApp
 
+//========================================================================
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+`;
+
 // Check theme provider.
 const theme = {
   colors: {
-    primary: '#0070f3'
-  }
+    primary: '#0070f3',
+  },
 };
 
 export default class MyApp extends App {
   render() {
-    const {Component, pageProps} = this.props;
+    const { Component, pageProps } = this.props;
 
     return (
+      <>
+        <GlobalStyle />
         <ThemeProvider theme={theme}>
           <Component {...pageProps} />
         </ThemeProvider>
+      </>
     );
   }
 }
