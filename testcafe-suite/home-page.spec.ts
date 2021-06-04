e9e28ad277e36090b0537401554bdd63ed82dc01
Annie Huang <annie.huang@coles.com.au>
2021-06-04 12:07:20 +1000

    Use standard sample test but still failed...

--- testcafe-suite/home-page.spec.ts
+++ testcafe-suite/home-page.spec.ts
@@ -1,1 +1,15 @@
-fixture('Accordion').page('http://localhost:4200/#/accordion');
+import { Selector } from 'testcafe'; // first import testcafe selectors
+
+fixture`Getting Started` // declare the fixture
+  .page`https://devexpress.github.io/testcafe/example`; // specify the start page
+
+//then create a test and place your code there
+test('My first test', async (t) => {
+  await t
+    .typeText('#developer-name', 'John Smith')
+    .click('#submit-button')
+
+    // Use the assertion to check if the actual header text is equal to the expected one
+    .expect(Selector('#article-header').innerText)
+    .eql('Thank you, John Smith!');
+});
